07e359d1c979bda271f4c0537565bdaeb54cca30
quantosapplications <info@quantos.it>
2021-05-04 12:18:59 +0200

    funciont cors

--- src/responses/cors.ts
+++ src/responses/cors.ts
@@ -7,3 +7,19 @@ export const corsConfig = {
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
     optionsSuccessStatus: 200
 };
+
+export function cors(req: any, res: any) {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD');
+    res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.set('Access-Control-Allow-Credentials', 'true');
+    res.set('Access-Control-Expose-Headers', 'Authorization');
+    if (req.method === 'OPTIONS') {
+        // Send response to OPTIONS requests
+        res.status(204);
+        res.setHeader('Content-Length', '0');
+        res.end();
+        return true;
+    }
+    return false;
+}
--- src/responses/index.ts
+++ src/responses/index.ts
@@ -1,5 +1,5 @@
 export { ApiError } from './api_error';
-export { corsConfig } from './cors';
+export { corsConfig, cors } from './cors';
 export { fireToJSON } from './firestore_to_json';
 export { getHttpStatus } from './http_status';
 export { success, failure } from './responses';
